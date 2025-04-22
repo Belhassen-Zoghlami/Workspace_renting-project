942ce954878401698d9c448edae32474e9ca7809
iyadh <iyadhche@gmail.com>
2025-04-22 10:00:45 +0100

    modified:   src/index.php

--- src/index.php
+++ src/index.php
@@ -1,5 +1,4 @@
 
-
 <?php
 
     define('__ROOT__', dirname(dirname(__FILE__)));
@@ -24,12 +23,33 @@ require_once(__ROOT__.'/src/navbar.php');
                 <section class="bg-black/50 h-screen snap-start body">
 
                     <h2 class="hidenh hero2 text-shadow-lg/40">
-                                Where we offer a wide variety of workspaces for you to explore,
+                        Welcome to Worki-n, your digital launchpad for discovering dynamic workspaces tailored to your flow
                             </h2>
                             <br><br>
                     <h2 class="hidenh hero2 text-shadow-lg/40">
-                                aswell as ensure peak productivity and creativity.
+                        Whether you're a solo hustler or a team force, this is your gateway to productive zones that match your energy.
                             </h2>
+                </section>
+            </div>
+            <div class="second-home-content bg-[url(../images/hero-op3.webp)]"> 
+                <section class="bg-black/50 h-screen snap-start body">
+
+                    <h2 class="hidenv hero2 text-shadow-lg/40">
+                        At Worki-n, we offer an expansive selection of work environments designed to inspire focus, innovation, and efficiency.
+                            </h2>
+                            <br><br>
+                    <h2 class="hidenv hero2 text-shadow-lg/40">
+                        From cozy desks to creative hubs, we’ve got the space — you bring the mission.,
+                            </h2>
+                </section>
+            </div>
+            <div class="second-home-content bg-[url(../images/hero-op4.webp)]"> 
+                <section class="bg-black/50 h-screen snap-start body">
+
+                    <h2 class="hidenh hero2 text-shadow-lg/40">
+                        Our mission is simple: fuel your productivity while keeping your creativity alive. With intuitive features and seamless access, Worki-n empowers you to do your best work, wherever you are
+                            </h2>
+                            <br><br>
                 </section>
             </div>
         </div>
@@ -46,13 +66,3 @@ require_once(__ROOT__.'/src/navbar.php');
 
 
 
-<!-- 
-    eaach room has:
-    -gallery: images
-    -location
-    -capacity
-    -equipement
-        -price/duration
-        -availability:
-            if booked-> show booked til when;
--->
